33f77cf67632d20c5b817046165c98c1bbdfe72d
root <root@ip-172-31-44-24.us-east-2.compute.internal>
2019-05-30 22:06:25 +0300

    Fix unittest.sh

--- unittest.sh
+++ unittest.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python2.7 kobyDate.py 999 &
+python2.7 kobyDate.py 2999 &
 
 sleep 3
 
-curl -s http://localhost:999/tikal | grep "KOBY TEST" > /dev/null
+curl -s http://localhost:2999/tikal | grep "KOBY TEST" > /dev/null
 if [ $? -ne 0 ] ; then
 	echo "FAILED!"
 	exit 1
